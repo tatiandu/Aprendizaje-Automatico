8fe9bc7f7a0e04933ce4575c830f4965f4b1bc7a
Miguel Mur Cortés <mmur@ucm.es>
2021-09-16 17:03:49 +0200

    Update P0.py
    Mi primer push del año

--- Practica0/P0.py
+++ Practica0/P0.py
@@ -1,25 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 16 16:07:39 2021
+
+@author: friki
+"""
+
+import time
 import numpy as np
-import time #para comparar los tiempos de ejecucion
+import matplotlib.pyplot as plt
 
+def dot_product(x1, x2):
+    """Calcula el producto escalar con un bucle
+    y devuelve el tiempo en milisegundos"""
+    tic = time.process_time()
+    dot = 0
+    for i in range(len(x1)):
+        dot += x1[1] * x2[i]
+    toc = time.process_time()
+    return 1000 * (toc - tic)
 
-#comprobar si es correcto con scipy.integrate.quad
-#comparar los tiempos de ejecucion de ambas versiones y ver cual es mejor
-
-
-#calcula la integral de fun entre a y b por el metodo de Monte Carlo generando num_puntos aleatorios
-
-#version iterativa que realiza num_puntos iteraciones para calcular el resultado
-def integra_mc_iterativo(fun, a, b, num_puntos=10000):
-    print("Iterativo\n") #TODO
+def fast_dot_product(x1, x2):
+    """Calcula el producto escalar vectorizado
+    y devuelve el tiempo en milisegundos"""
+    tic = time.process_time()
+    dot = np.dot(x1, x2)
+    toc = time.process_time()
+    return 1000 * (toc - tic)
     
+def compara_tiempos():
+    sizes = np.linspace(100, 10000000, 20)
+    times_dot = []
+    times_fast = []
+    for size in sizes:
+        x1 = np.random.uniform(1, 100, int(size))
+        x2 = np.random.uniform(1, 100, int(size))
+        times_dot += [dot_product(x1, x2)]
+        times_fast += fast_dot_product(x1, x2)
+    plt.figure()
+    plt.scatter(sizes, times_dot, c='red', label='bucle')
+    plt.scatter(sizes, times_fast, c='blue', label='vector')
+    plt.legend()
+    plt.savefig('time.png')
 
-#version que utilice operaciones entre vectores en lugar de bucles
-def integra_mc_vectorizado(fun, a, b, num_puntos=10000):
-    print("Vectorizado\n") #TODO
-
-
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-def main():
-    print("Main\n") #TODO
-
-main()
+def cuadratica(x):
+    return x * x
+    
+def integra_mc(fun, a, b, num_puntos=10000):
+    'Cálculo de M'
+    intervalo = np.linspace(a, b, num_puntos)
+    M = cuadratica(intervalo).max()
+    'Calculamos num_puntos aleatorios'
+    ran_x = np.random.rand()
+integra_mc(cuadratica, 0, 50)
