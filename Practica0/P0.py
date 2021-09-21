ee2ddc01d6b9c0e995ecd95e6ca6e2eaeaf89879
Miguel Mur Cortés <mmur@ucm.es>
2021-09-21 23:33:51 +0200

    finiquitado

--- Practica0/P0.py
+++ Practica0/P0.py
@@ -2,49 +2,37 @@
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.integrate as spicy
-
-def dot_product(x1, x2):
-    """Calcula el producto escalar con un bucle
-    y devuelve el tiempo en milisegundos"""
-    tic = time.process_time()
-    dot = 0
-    for i in range(len(x1)):
-        dot += x1[1] * x2[i]
-    toc = time.process_time()
-    return 1000 * (toc - tic)
-
-def fast_dot_product(x1, x2):
-    """Calcula el producto escalar vectorizado
-    y devuelve el tiempo en milisegundos"""
-    tic = time.process_time()
-    dot = np.dot(x1, x2)
-    toc = time.process_time()
-    return 1000 * (toc - tic)
-    
-def compara_tiempos_():
-    sizes = np.linspace(100, 10000000, 20)
-    times_dot = []
-    times_fast = []
-    for size in sizes:
-        x1 = np.random.uniform(1, 100, int(size))
-        x2 = np.random.uniform(1, 100, int(size))
-        times_dot += [dot_product(x1, x2)]
-        times_fast += fast_dot_product(x1, x2)
-    plt.figure()
-    plt.scatter(sizes, times_dot, c='red', label='bucle')
-    plt.scatter(sizes, times_fast, c='blue', label='vector')
-    plt.legend()
-    plt.savefig('time.png')
-
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 def cuadratica(x):
     return x * x
 
-def sin_function(x):
-    return np.sin(x)
+def presenta_grafica_tiempo(fun, a, b, num_puntos=30):
+    intervalo = np.linspace(100, 1000000, num_puntos)
     
+    time_it = []
+    time_vec = []
+    
+    for i in intervalo:
+        time_it += [integra_mc_iterativo(fun, a, b, int(i))[1]]
+        time_vec += [integra_mc_vectorizado(fun, a, b, int(i))[1]]
+    
+    plt.figure()
+    plt.scatter(intervalo, time_it, c='red', label='t. bucle')
+    plt.scatter(intervalo, time_vec, c='blue', label='t. vector')
+    plt.legend()
+    plt.savefig('time.png')
+    
+def presenta_grafica_integral(fun, a, b, num_puntos=10000):
+    intervalo = np.linspace(a, b, num_puntos)
+    M = np.max(fun(intervalo))
+    plt.figure()
+    plt.plot(intervalo, fun(intervalo), '-', c='purple', label='x^2')
+    ran_x = np.random.rand(num_puntos) * (b-a) + a
+    ran_y = np.random.rand(num_puntos) * M
+    plt.scatter(ran_x, ran_y, marker='.', c='red', label='random points')
+    plt.legend()
+    plt.savefig('integral.png')
+
 def integra_mc_iterativo(fun, a, b, num_puntos=10000):
     tic = time.process_time()
     #Calculo de M
@@ -71,19 +59,19 @@ def integra_mc_vectorizado(fun, a, b, num_puntos=10000):
     ran_x = np.random.rand(num_puntos) * (b-a) + a
     ran_y = np.random.rand(num_puntos) * M
     num_debajo = np.sum(fun(ran_x) > ran_y)
-    print(num_debajo)
     #Calculo de la integral
     integral = (num_debajo/num_puntos) * (b-a) * M
     toc = time.process_time()
     return (integral, (toc-tic)*1000)
 
 def compara_tiempos():
-    resultado_iterativo = integra_mc_iterativo(cuadratica, 1, 20, 10000000)
-    resultado_vectorizado = integra_mc_vectorizado(cuadratica, 1, 20, 10000000)
-    print("Tiempo para proceso iterativo:",resultado_iterativo[1])
+    resultado_iterativo = integra_mc_iterativo(cuadratica, 1, 20, 1000000)
+    resultado_vectorizado = integra_mc_vectorizado(cuadratica, 1, 20, 1000000)
+    print("Tiempo para proceso iterativo en ms:",resultado_iterativo[1])
     print("Resultado de la integral:",resultado_iterativo[0])
-    print("Tiempo para proceso vectorizado:",resultado_vectorizado[1])
+    print("Tiempo para proceso vectorizado en ms:",resultado_vectorizado[1])
     print("Resultado de la integral:",resultado_vectorizado[0])
-    print(spicy.quad(cuadratica, 1, 20))
-    
+    presenta_grafica_integral(cuadratica, 1, 20, 1000)
+    presenta_grafica_tiempo(cuadratica, 1, 20, 30)
+
 compara_tiempos()
