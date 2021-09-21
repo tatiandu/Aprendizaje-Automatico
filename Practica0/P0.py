ce769e38780beecfb6bf4c15cd243f07d08a2d5b
Miguel Mur Cortés <mmur@ucm.es>
2021-09-21 16:31:54 +0200

    parece que va

--- Practica0/P0.py
+++ Practica0/P0.py
@@ -2,6 +2,7 @@
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.integrate as spicy
 
 def dot_product(x1, x2):
     """Calcula el producto escalar con un bucle
@@ -41,11 +42,14 @@ def compara_tiempos_():
 def cuadratica(x):
     return x * x
 
+def sin_function(x):
+    return np.sin(x)
+    
 def integra_mc_iterativo(fun, a, b, num_puntos=10000):
     tic = time.process_time()
     #Calculo de M
     intervalo = np.linspace(a, b, num_puntos)
-    M = fun(intervalo).max()
+    M = np.max(fun(intervalo))
     #Calculamos num_puntos aleatorios
     num_debajo = 0
     for i in range(num_puntos):
@@ -56,22 +60,30 @@ def integra_mc_iterativo(fun, a, b, num_puntos=10000):
     #Calculo de la integral
     integral = (num_debajo/num_puntos) * (b-a) * M
     toc = time.process_time()
-    return (integral, (toc-tic)*1000)
+    return (integral, (toc-tic) * 1000)
 
 def integra_mc_vectorizado(fun, a, b, num_puntos=10000):
     tic = time.process_time()
     #Calculo de M
     intervalo = np.linspace(a, b, num_puntos)
-    M = fun(intervalo).max()
+    M = np.max(fun(intervalo))
     #Calculamos num_puntos aleatorios
     ran_x = np.random.rand(num_puntos) * (b-a) + a
     ran_y = np.random.rand(num_puntos) * M
-    num_debajo = sum(fun(ran_x) > ran_y)
+    num_debajo = np.sum(fun(ran_x) > ran_y)
+    print(num_debajo)
     #Calculo de la integral
     integral = (num_debajo/num_puntos) * (b-a) * M
     toc = time.process_time()
     return (integral, (toc-tic)*1000)
 
 def compara_tiempos():
-    print("a")
-
+    resultado_iterativo = integra_mc_iterativo(cuadratica, 1, 20, 10000000)
+    resultado_vectorizado = integra_mc_vectorizado(cuadratica, 1, 20, 10000000)
+    print("Tiempo para proceso iterativo:",resultado_iterativo[1])
+    print("Resultado de la integral:",resultado_iterativo[0])
+    print("Tiempo para proceso vectorizado:",resultado_vectorizado[1])
+    print("Resultado de la integral:",resultado_vectorizado[0])
+    print(spicy.quad(cuadratica, 1, 20))
+    
+compara_tiempos()
