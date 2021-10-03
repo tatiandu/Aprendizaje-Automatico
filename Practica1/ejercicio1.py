b5b7283683c2e7fb2eac6c2468f2b2b31d982ca6
Tatiana <tatiandu@ucm.es>
2021-10-03 19:14:19 +0200

    kk

--- Practica1/ejercicio1.py
+++ Practica1/ejercicio1.py
@@ -7,8 +7,9 @@ def carga_csv(file_name):
     valores = read_csv(file_name,  header=None).to_numpy()
     return valores.astype(float)
 
-def descenso_gradiente(theta0, theta1, x, y, alpha = 0.01, num_it = 1500):
+def descenso_gradiente(x, y, alpha = 0.01, num_it = 1500):
     m = len(x)
+    theta0 = theta1 = 0
 
     for aux in range(num_it):
         sum0 = sum1 = 0
@@ -40,18 +41,19 @@ def make_data(rangoT0, rangoT1, x, y):
     for ix, iy in np.ndindex(theta0.shape): #Itera por todas las dimensiones de la matriz
         costeF[ix, iy] = coste(x, y, [theta0[ix, iy], theta1[ix, iy]])
 
-    return [theta0, theta1, costeF] 
+    return (theta0, theta1, costeF)
 
 
 def regresion_lineal_una_variable():
     datos = carga_csv("ex1data1.csv")
     x = datos[:, 0]
     y = datos[:, 1]
-    theta0 = theta1 = 0
 
-    thetas = descenso_gradiente(theta0, theta1 , x, y)
+    thetas = descenso_gradiente(x, y)
 
-    plt.plot(x, y, "x", c="orange") #TODO: dar nombre a los ejes
+    plt.xlabel("Población de la ciudad en 10 000s")
+    plt.ylabel("Ingresos en $10 000s")
+    plt.plot(x, y, "x", c="orange")
     minX = min(x)
     maxX = max(x)
     minY = thetas[0] + thetas[1] * minX
@@ -68,27 +70,53 @@ def regresion_lineal_una_variable():
     plt.show()
     plt.savefig("resultado2.png")
 
-def matriz_norm(matriz):
-    nCols = matriz.shape[1]
-    matriz_norm = np.empty_like(matriz)
-    mu = np.empty_like(matriz[0])
-    sigma = np.empty_like(matriz[0])
-    for n in range(nCols):
-        mu[n] = np.median(matriz[:, n])
-        sigma[n] = np.std(matriz[:, n])
-        sum = np.linalg.norm
-        matriz_norm[:,n] = (matriz[:,n] / (np.sum(matriz[:, n])))
-    return (matriz_norm, mu, sigma)
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+def matriz_norm(x):
+    mu = np.mean(x, axis=0)
+    sigma = np.std(x, axis=0)
+    x_norm = (x - mu) / sigma
+    return (x_norm, mu, sigma)
+
+
+# def coste_vect(x, y, theta):
+#     aux = np.dot(x, theta) - y
+#     auxT = np.transpose(aux)
+#     return (np.dot(auxT, aux)) / (2*len(x))
+
+
+def descenso_gradiente_vect(x, y, alpha):
+    thetas = np.empty_like(x[1])
+    auxThetas = thetas
+    costes = list()
+
+    for i in range(1500):
+        h = np.dot(x, thetas)
+        sum = h - y
+        auxThetas = thetas - alpha/len(x) * np.dot(sum, x)
+        thetas = auxThetas
+        costes.append((sum**2).sum() / (2*len(x)))
+
+    return thetas, costes
+
 
 def regresion_varias_variables():
     datos = carga_csv("ex1data2.csv")
-    datos_norm, mu, sigma = matriz_norm(datos)
-    X = datos_norm[:, :]
-    X = datos_norm[:, :-1]
-    Y = datos_norm[:, -1]
-    m = np.shape(X)[0]
-    n = np.shape(X)[1]
-    X = np.hstack([np.ones([m,1]),X])
-    return 0
+    x = datos[:, :-1]
+    y = datos[:, -1]
+
+    x_norm, mu, sigma = matriz_norm(x)
+    x_norm = np.hstack([np.ones([np.shape(x)[0], 1]), x_norm])
+
+    plt.figure()
+    alphas = [0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1]
+    for a in alphas:
+        thetas, costes = descenso_gradiente_vect(x_norm, y, a)
+        plt.plot(np.arange(1500) + 1, costes, label = a)
+
+    plt.legend()
+    plt.savefig("resultadoAlphas.png")
+
+
 
 regresion_varias_variables()
