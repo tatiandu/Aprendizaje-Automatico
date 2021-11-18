5d8b0e7fe95803ac34bc6b06dce5495918c77a13
Tatiana <tatiandu@ucm.es>
2021-11-18 13:13:01 +0100

    avances p5

--- Practica5/P5.py
+++ Practica5/P5.py
@@ -1,7 +1,7 @@
 from scipy.io import loadmat
 import scipy.optimize as opt
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 
 
 def coste(Theta, X, y, lamda):
@@ -19,7 +19,25 @@ def gradiente(Theta, X, y, lamda):
     return op1
 
 
-def grafica_reg_lineal(Theta, X, y):
+def regresion_lineal_reg(X, y):
+    lamda = 1
+    Theta = np.array([1,1])
+    X_aux = np.hstack([np.ones([np.shape(X)[0], 1]), X])
+
+    coste_ini = coste(Theta, X_aux, y, lamda)
+    gradiente_ini = gradiente(Theta, X_aux, y, lamda)
+    print("---Regresión lineal regularizada---")
+    print(f"Coste inicial: {str(coste_ini)[:7]}")
+    print(f"Gradiente inicial: [{str(gradiente_ini[0])[:7]}; {str(gradiente_ini[1])[:7]}]")
+    print()
+
+    #Calculo del valor de Theta que minimiza el error sobre los ejemplos de entrenamiento
+    lamda = 0
+    fmin = opt.minimize(fun=coste, x0=Theta, args=(X_aux, y, lamda))
+    grafica_regresion_lineal_reg(fmin.x, X, y)
+
+
+def grafica_regresion_lineal_reg(Theta, X, y):
     plt.figure()
     plt.plot(X, y, "x", c="orange")
 
@@ -34,6 +52,65 @@ def grafica_reg_lineal(Theta, X, y):
     plt.ylabel("Water flowing out of the dam (y)")
     plt.savefig("figura1.png")
 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+def error(Theta, X, y):
+    H = np.dot(X, Theta)
+    return np.sum((H - y)**2) / (2 * np.shape(X)[0])
+
+
+def curvas_aprendizaje(X, Xval, y, yval):
+    m = np.shape(X)[0] 
+    lamda = 0
+    Theta = np.array([1,1])
+    X_aux = np.hstack([np.ones([m, 1]), X])
+    Xval_aux = np.hstack([np.ones([np.shape(Xval)[0], 1]), Xval])
+
+    errors = []
+    errorsval = []
+
+    for i in range(1, m+1):
+        fmin = opt.minimize(fun=coste, x0=Theta, args=(X_aux[:i], y[:i], lamda))
+        errors.append(error(fmin.x, X_aux[:i], y[:i]))
+        errorsval.append(error(fmin.x, Xval_aux, yval))
+    
+    grafica_curvas_aprendizaje(m, errors, errorsval)
+
+
+def grafica_curvas_aprendizaje(m, errors, errorsval):
+    plt.figure()
+
+    plt.plot(range(1, m+1), errors, c="orange", label="Train")
+    plt.plot(range(1, m+1), errorsval, c="limegreen", label="Cross Validation")
+
+    plt.legend()
+    plt.title("Learning curve for linear regression")
+    plt.xlabel("Number of training examples")
+    plt.ylabel("Error")
+    plt.savefig("figura2.png")
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+def normaliza_matriz(X):
+    mu = np.mean(X, axis = 0)
+    sigma = np.std(X, axis = 0)
+    X_norm = (X - mu)/sigma
+    return X_norm, mu, sigma
+
+
+def genera_datos(X, p):
+    H = np.empty((np.shape(X)[0], p))
+
+    for i in range(0, p):
+        H[:,i] = (X**(i+1)).ravel()
+
+    #print(np.shape(H))
+    return H
+
+
+#def regresion_polinomial():
+
+
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -42,20 +119,8 @@ def main():
     X, Xval, Xtest = datos["X"], datos["Xval"], datos["Xtest"]
     y, yval, ytest = datos["y"].ravel(), datos["yval"].ravel(), datos["ytest"].ravel()
 
-    lamda = 1
-    Theta = np.array([1,1])
-    print(Theta)
-    X_aux = np.hstack([np.ones([np.shape(X)[0], 1]), X])
-
-    coste_ini = coste(Theta, X_aux, y, lamda)
-    gradiente_ini = gradiente(Theta, X_aux, y, lamda)
-    print(f"Coste inicial: {str(coste_ini)[:7]}")
-    print(f"Gradiente inicial: [{str(gradiente_ini[0])[:7]}; {str(gradiente_ini[1])[:7]}]")
-
-    #Calculo del valor de Theta que minimiza el error sobre los ejemplos de entrenamiento
-    lamda = 0
-    fmin = opt.minimize(fun=coste, x0=Theta, args=(X_aux, y, lamda))
-    grafica_reg_lineal(fmin.x, X, y)
+    #regresion_lineal_reg(X, y)
+    #curvas_aprendizaje(X, Xval, y, yval)
 
 
 
